40d8f587f11bede0e721f39aff7f777fe8f9056b
khoido2003 <khoizpro31@gmail.com>
2025-08-29 18:22:16 +0700

    feat: enhance README with new configuration options and update rainbow delimiters color logic

--- lua/monokai-v2/theme/plugins/rainbow-delimiters.lua
+++ lua/monokai-v2/theme/plugins/rainbow-delimiters.lua
@@ -3,10 +3,10 @@ local M = {}
 --- @param c Colorscheme The color palette
 function M.get(c, _, _)
   return {
-    RainbowDelimiterRed = { fg = c.base.red },
+    RainbowDelimiterRed = { fg = c.punctuation.delimiter.pink or c.base.red },
     RainbowDelimiterYellow = { fg = c.punctuation.delimiter.yellow or c.base.yellow },
     RainbowDelimiterBlue = { fg = c.punctuation.delimiter.blue or c.base.cyan },
-    RainbowDelimiterOrange = { fg = c.punctuation.delimiter.orange or c.base.blue },
+    RainbowDelimiterOrange = { fg = c.punctuation.delimiter.orange or c.base.yellow },
     RainbowDelimiterGreen = { fg = c.punctuation.delimiter.green or c.base.green },
     RainbowDelimiterViolet = { fg = c.punctuation.delimiter.purple or c.base.magenta },
     RainbowDelimiterCyan = { fg = c.punctuation.delimiter.cyan or c.base.cyan },
