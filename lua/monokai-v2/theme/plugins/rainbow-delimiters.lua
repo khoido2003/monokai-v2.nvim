c9b688ef0238670500944342da2e2b316b1fb2cc
khoido2003 <khoizpro31@gmail.com>
2026-02-04 12:54:36 +0700

    fix(rainbow-delimiter): fix fallback color

--- lua/monokai-v2/theme/plugins/rainbow-delimiters.lua
+++ lua/monokai-v2/theme/plugins/rainbow-delimiters.lua
@@ -3,10 +3,10 @@ local M = {}
 --- @param c Colorscheme The color palette
 function M.get(c, _, _)
   return {
-    RainbowDelimiterRed = { fg = c.punctuation.delimiter.pink or c.base.red },
+    RainbowDelimiterRed = { fg = c.punctuation.delimiter.pink or c.base.pink },
     RainbowDelimiterYellow = { fg = c.punctuation.delimiter.yellow or c.base.yellow },
     RainbowDelimiterBlue = { fg = c.punctuation.delimiter.blue or c.base.cyan },
-    RainbowDelimiterOrange = { fg = c.punctuation.delimiter.orange or c.base.yellow },
+    RainbowDelimiterOrange = { fg = c.punctuation.delimiter.orange or c.base.orange },
     RainbowDelimiterGreen = { fg = c.punctuation.delimiter.green or c.base.green },
     RainbowDelimiterViolet = { fg = c.punctuation.delimiter.purple or c.base.purple },
     RainbowDelimiterCyan = { fg = c.punctuation.delimiter.cyan or c.base.cyan },
